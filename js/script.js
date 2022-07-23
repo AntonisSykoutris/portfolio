c2bd02a4d71a17052306c55ab6eb8069709262bb
Antonis Sykoutris CSD3848 <csd3848@csd.uoc.gr>
2022-07-23 13:42:29 +0300

    update

--- js/script.js
+++ js/script.js
@@ -128,19 +128,15 @@ const checkFlexGap = () => {
 checkFlexGap();
 
 function reveal() {
-  var reveals = document.querySelectorAll(".reveal");
+  const reveals = document.querySelectorAll(".reveal");
+  reveal.forEach((el, i) => {
+    const windowHeight = window.innerHeight;
+    const elementTop = reveals[i].getBoundingClientRect().top;
+    const elementVisible = 150;
 
-  for (var i = 0; i < reveals.length; i++) {
-    var windowHeight = window.innerHeight;
-    var elementTop = reveals[i].getBoundingClientRect().top;
-    var elementVisible = 150;
-
-    if (elementTop < windowHeight - elementVisible) {
-      reveals[i].classList.add("active");
-    } else {
-      reveals[i].classList.remove("active");
-    }
-  }
+    if (elementTop < windowHeight - elementVisible) reveals[i].classList.add("active");
+    if (elementTop >= windowHeight - elementVisible) reveals[i].classList.remove("active");
+  });
 }
 
 window.addEventListener("scroll", reveal);
